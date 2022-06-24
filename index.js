59d8a2108852c898596599afd51e1a2302dc86f1
Saint-Josh <saintjosh26@gmail.com>
2022-06-24 02:44:22 +0100

    Adding a custom function

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const defaultResult = 0;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return 'The result is ' + result;
+}
+
+console.log(add(1, 2));
+console.log(add(5, 5));
 let currentResult = defaultResult; //copy of the const var above
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
