e740a8f1ed7dd1c299a4a964cb447e1eb952a133
Saint-Josh <saintjosh26@gmail.com>
2022-06-24 01:52:58 +0100

    using constants

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult; //copy of the const var above
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
-let currentDescription = '(' + currentResult + ' * 10 ) * 3 / 2 - 1';
+let currentDescription = '(' + defaultResult + ' * 10 ) * 3 / 2 - 1';
 console.log(currentResult, currentDescription);
