bc997eac5eb6eb688568094c6725d7df1d1b91f9
Saint-Josh <saintjosh26@gmail.com>
2022-06-24 01:48:23 +0100

    working with variables and operators

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+console.log(currentResult, '');
